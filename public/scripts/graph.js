8b50ababf3ef456f83a2b57c7e922c817c358d8c
tanxx519 <tanxx519@umn.edu>
2021-02-03 22:18:57 -0600

    added graphs

--- public/scripts/graph.js
+++ public/scripts/graph.js
@@ -4,57 +4,67 @@ const countryName = document
 
 const api_url = `https://disease.sh/v3/covid-19/historical/${countryName}?lastdays=all`;
 
-const timeline = [];
-const confirmed = [];
-const death = [];
-const recovered = [];
+const timelineArr = [];
+const confirmedArr = [];
+const deathArr = [];
+const recoveredArr = [];
 
-async function getData() {
-	const response = await fetch(api_url);
-	const data = await response.json();
-	console.log(data);
-}
+const pushData = function (data, array) {
+	data.forEach(function (newData) {
+		array.push(newData[1]);
+	});
+};
 
-getData();
+const pushTime = function (data, array) {
+	data.forEach(function (newData) {
+		array.push(newData[0]);
+	});
+};
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-	type: "bar",
-	data: {
-		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-		datasets: [
-			{
-				label: "# of Votes",
-				data: [12, 19, 3, 5, 2, 3],
-				backgroundColor: [
-					"rgba(255, 99, 132, 0.2)",
-					"rgba(54, 162, 235, 0.2)",
-					"rgba(255, 206, 86, 0.2)",
-					"rgba(75, 192, 192, 0.2)",
-					"rgba(153, 102, 255, 0.2)",
-					"rgba(255, 159, 64, 0.2)",
-				],
-				borderColor: [
-					"rgba(255, 99, 132, 1)",
-					"rgba(54, 162, 235, 1)",
-					"rgba(255, 206, 86, 1)",
-					"rgba(75, 192, 192, 1)",
-					"rgba(153, 102, 255, 1)",
-					"rgba(255, 159, 64, 1)",
-				],
-				borderWidth: 1,
-			},
-		],
-	},
-	options: {
-		scales: {
-			yAxes: [
+const getData = async function () {
+	await fetch(api_url)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			const cases = Object.entries(data.timeline.cases);
+			const deaths = Object.entries(data.timeline.deaths);
+			const recovered = Object.entries(data.timeline.recovered);
+			pushData(cases, timelineArr);
+			pushData(cases, confirmedArr);
+			pushData(deaths, deathArr);
+			pushData(recovered, recoveredArr);
+		});
+};
+
+async function plotData() {
+	await getData();
+	var ctx = document.getElementById("myChart").getContext("2d");
+	var myChart = new Chart(ctx, {
+		type: "line",
+		data: {
+			labels: timelineArr,
+			datasets: [
 				{
-					ticks: {
-						beginAtZero: true,
-					},
+					label: `# of confirmed`,
+					data: confirmedArr,
+					borderWidth: 1,
+				},
+
+				{
+					label: `# of death`,
+					data: deathArr,
+					borderWidth: 1,
+				},
+
+				{
+					label: `# of recovered`,
+					data: recoveredArr,
+					borderWidth: 1,
 				},
 			],
 		},
-	},
-});
+	});
+}
+
+plotData();
