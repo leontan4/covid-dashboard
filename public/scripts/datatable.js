819b93c40af17d527d309e8095d84c1087bb044b
tanxx519 <tanxx519@umn.edu>
2021-02-11 18:08:36 -0600

    updated table views

--- public/scripts/datatable.js
+++ public/scripts/datatable.js
@@ -1,5 +1,8 @@
-$(".dataTable").DataTable({
-	pageLength: 25,
-	lengthMenu: [10, 25, 50, 75, 100],
-	bSort: false,
+$(document).ready(function () {
+	$(".dataTable").DataTable({
+		pageLength: 25,
+		lengthMenu: [10, 25, 50, 75, 100],
+		order: [[1, "desc"]],
+		responsive: true,
+	});
 });
