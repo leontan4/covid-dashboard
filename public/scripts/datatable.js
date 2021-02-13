307a4d2927b86b31dbd7eaa311f1c17cdc048414
tanxx519 <tanxx519@umn.edu>
2021-02-13 14:34:29 -0600

    removed pointers

--- public/scripts/datatable.js
+++ public/scripts/datatable.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
 		pageLength: 25,
 		lengthMenu: [10, 25, 50, 75, 100],
 		order: [[1, "desc"]],
+		sScrollX: "100%",
+		scrollCollapse: true,
 		responsive: true,
-		scrollX: true,
+		fixedColumns: {
+			leftColumns: 1,
+			heightMatch: "semiauto",
+		},
 	});
 });
