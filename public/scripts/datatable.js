006116ccf2bec1a7413d0944e8c75af1bb419226
tanxx519 <tanxx519@umn.edu>
2021-02-12 22:25:20 -0600

    added scrollX

--- public/scripts/datatable.js
+++ public/scripts/datatable.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-	$(".dataTable").DataTable({
+	$("#dataTable").DataTable({
 		pageLength: 25,
 		lengthMenu: [10, 25, 50, 75, 100],
 		order: [[1, "desc"]],
 		responsive: true,
+		scrollX: true,
 	});
 });
